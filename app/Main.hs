61e7e2f08d5e61abe01b5030ef06e7b482050770
Robert Kreuzer <robert.kreuzer@gmail.com>
2017-04-27 22:27:40 +0200

    Place the first word on the playing field

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,33 @@
 module Main where
 
 import Trie
+import qualified Data.Map as Map
 
+
+type Coordinates = (Int, Int)
+
+-- each playing piece is simply a character
+type PlayingField = Map.Map Coordinates Char
+
+someWords :: [String]
+someWords = ["just", "some", "random", "words", "without", "meaning"]
+
+
+placeFirstWord' :: String -> PlayingField -> Coordinates -> PlayingField
+placeFirstWord' [] pf _         = pf
+placeFirstWord' (z:zs) pf (x,y) = placeFirstWord' zs newPlayingField (x, y+1)
+  where newPlayingField = Map.insert (x,y) z pf
+
+-- we always place the first word starting from (1,1) to the right
+placeFirstWord :: String -> PlayingField -> PlayingField
+placeFirstWord [] pf = pf
+placeFirstWord xs pf = placeFirstWord' xs pf (1,1)
 
 main :: IO ()
 main = do
-  let trie = insert "asd" (insert "asdf" emptyTrie)
+  let trie = foldl (flip insert) emptyTrie someWords
+      firstWord = head someWords
+      playingField = placeFirstWord firstWord Map.empty
   putStrLn $ "A trie: " ++ show trie
   putStrLn $ "All prefixes: " ++ show (allPrefixes trie)
+  putStrLn $ "The playing field: " ++ show playingField
