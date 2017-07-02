77d71f26b14131898dca0d4c7aec83d1f9b7a296
Robert Kreuzer <robert.kreuzer@gmail.com>
2017-07-02 20:56:49 +0200

    Move almost everything to Lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,6 @@
 module Lib (
+  getAllPlacementOptions,
+  getAvailablePlacedPieces,
   insertPlacedPieces,
   optionsInfo,
   placeFirstWord,
@@ -68,3 +70,128 @@ insertPlacedPieces (pp@(PlacedPiece c cs):pps) pf = case Map.lookup cs pf of
   Just pp' -> case pp == pp' of
     True  -> insertPlacedPieces pps (Map.insert cs pp pf)
     False -> error $ "The given piece " ++ show pp ++ " is invalid. " ++ show pp'
+
+
+-- get all placed pieces where a word could be attached, i.e.
+-- all pieces where either the x or the y-axis is still free
+getAvailablePlacedPieces :: PlayingField -> [PlacedPiece]
+getAvailablePlacedPieces pf = filter (isPieceAvailable pf) (Map.elems pf)
+
+
+-- a piece is available if either its left and right or upper and lower
+-- neigbors are free
+isPieceAvailable :: PlayingField -> PlacedPiece -> Bool
+isPieceAvailable pf (PlacedPiece c cs) = isVerticalFree || isHorizontalFree
+  where
+    neighbors = getNeighbors pf cs
+    isVerticalFree   = emptyNeighbors cs [Up, Down] pf
+    isHorizontalFree = emptyNeighbors cs [L, R] pf
+
+
+getNeighbors :: PlayingField -> Coordinates -> Map.Map Direction (Maybe PlacedPiece)
+getNeighbors pf (x,y) = Map.map (\cs -> Map.lookup cs pf) neighborMap
+  where
+    neighborCoordinates = [(L, (x-1, y)), (R, (x+1, y)), (Up, (x, y+1)), (Down, (x, y-1))]
+    neighborMap = Map.fromList neighborCoordinates
+
+
+-- check if the neighbors in the given directions are all empty
+emptyNeighbors :: Coordinates -> [Direction] -> PlayingField -> Bool
+emptyNeighbors cs ds pf = all (isNeighborEmpty cs pf) ds
+
+
+isNeighborEmpty :: Coordinates -> PlayingField -> Direction -> Bool
+isNeighborEmpty cs pf d = Map.notMember (goOne d cs) pf
+
+
+goOne :: Direction -> Coordinates -> Coordinates
+goOne Up   (x,y) = (x, y+1)
+goOne Down (x,y) = (x, y-1)
+goOne L    (x,y) = (x-1, y)
+goOne R    (x,y) = (x+1, y)
+
+
+-- check if the given word does not touch or overlap with conflicting pieces
+isWordFitting :: PlayingField -> (MoveType, [PlacedPiece]) -> Bool
+isWordFitting pf (_, [])   = True
+-- TODO: Placed pieces are *not* ordered
+isWordFitting pf (mt, (pp@(PlacedPiece c cs):pps)) =
+  isFirstValid && (all (isEqualOrEmpty pf mt) (init pps)) && isLastValid
+  where
+    -- Note: If the field is empty, then all three surrounding fields must be empty
+    -- If the field is not empty, then only the field in the same direction must be empty
+    isFieldValid (PlacedPiece c cs) d ds = case Map.lookup cs pf of
+      Just (PlacedPiece c' cs') -> c == c' && isNeighborEmpty cs pf d
+      Nothing                   -> emptyNeighbors cs ds pf
+    isFirstValid = case mt of
+      Horizontal -> isFieldValid pp L [Up, Down, L]
+      Vertical   -> isFieldValid pp Down [L, R, Down]
+    isLastValid  = case mt of
+      Horizontal -> isFieldValid (last pps) R [Up, Down, R]
+      Vertical   -> isFieldValid (last pps) Up [L, R, Up]
+
+
+isEqualOrEmpty :: PlayingField -> MoveType -> PlacedPiece -> Bool
+isEqualOrEmpty pf mt (PlacedPiece c cs) = case Map.lookup cs pf of
+  Nothing -> case mt of
+    Horizontal -> emptyNeighbors cs [Up, Down] pf
+    Vertical   -> emptyNeighbors cs [L, R] pf
+  Just (PlacedPiece c' cs') -> case mt of
+    Horizontal -> c == c' && emptyNeighbors cs' [L, R] pf
+    Vertical   -> c == c' && emptyNeighbors cs' [Up, Down] pf
+
+
+-- For the given word, return *all* possible placements given the contraints of the playing field
+getFittingWords :: PlayingField -> String -> PlacedPiece -> [[PlacedPiece]]
+getFittingWords pf [] pp = []
+getFittingWords pf word pp = map snd fittingPlacements
+  where
+    possiblePlacements = getAllPossiblePlacements word pp
+    fittingPlacements = filter (isWordFitting pf) possiblePlacements
+
+
+-- It's often possible to attach a word in several ways to a given piece
+-- think e.g. about words that contain the character of the placed piece several times
+getAllPossiblePlacements :: String -> PlacedPiece -> [(MoveType, [PlacedPiece])]
+getAllPossiblePlacements word pp = horizontalPlacements ++ verticalPlacements
+  where
+    horizontalPlacements = zip (repeat Horizontal) $ getDirectonalPlacements word Horizontal pp
+    verticalPlacements   = zip (repeat Vertical) $ getDirectonalPlacements word Vertical pp
+
+
+getDirectonalPlacements :: String -> MoveType -> PlacedPiece -> [[PlacedPiece]]
+getDirectonalPlacements word mt pp = getDirectonalPlacements' [] word mt pp
+
+
+-- Note: We are generating all theoretical options here, they may not be possible
+-- in practice, if there are already different pieces on the playing field
+getDirectonalPlacements' :: String -> String -> MoveType -> PlacedPiece -> [[PlacedPiece]]
+getDirectonalPlacements' prefix [] mt pp@(PlacedPiece c cs) = []
+getDirectonalPlacements' prefix (x:xs) mt pp@(PlacedPiece c cs) = case x == c of
+  -- skip characters that don't match
+  False -> getDirectonalPlacements' (prefix ++ [x]) xs mt pp
+  True  -> case mt of
+    -- place the prefix to the left and the suffix to the right of (x,y)
+    -- we have to reverse twice: Once to insert in single steps and once to make the order right again
+    Horizontal -> let placedPrefix = placeString L (reverse prefix) pp
+                      pps = reverse placedPrefix ++ [pp] ++ placeString R xs pp
+                  in pps : getDirectonalPlacements' (prefix ++ [x]) xs mt pp
+                  -- Note: Down is Up and Up is Down, thanks to SVG's weird coordinate system
+    Vertical   -> let placedPrefix = placeString Down (reverse prefix) pp
+                      pps = reverse placedPrefix ++ [pp] ++ placeString Up xs pp
+                  in pps : getDirectonalPlacements' (prefix ++ [x]) xs mt pp
+
+
+placeString :: Direction -> String -> PlacedPiece -> [PlacedPiece]
+placeString _ [] _ = []
+placeString d (w:ws) (PlacedPiece c cs) = pp' : placeString d ws pp'
+  where
+    cs' = goOne d cs
+    pp' = PlacedPiece w cs'
+
+
+-- find all possible placements for the given word and the given placed piece
+getAllPlacementOptions :: String -> PlayingField -> PlacedPiece -> [(String, [PlacedPiece])]
+getAllPlacementOptions w pf pp = map (\pps -> (w, pps)) $ getFittingWords pf w pp
+
+
